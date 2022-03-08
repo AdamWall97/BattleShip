e40e2a282231bba0db487a60c9ccd65ed6da8151
Adam W <adamwall97@gmail.com>
2022-03-08 11:32:14 -0600

    Add Ship functionality

---
+++ src/Ship.js
@@ -0,0 +1,22 @@
+const projectFactory = (length) => {
+
+    var hitChart = Array(length).fill("");
+    var isSunk = false;
+
+    const getLength = () => length;
+    const getisSunk = () => isSunk;
+
+    const hit = (location) => {
+        hitChart[location] = "x";
+    };
+
+    const checkSink = () => {
+        for (let i =0; i < hitChart.length;i++){
+            if(hitchart[i] == ""){
+                return false;
+            }
+        }
+        return true;
+    };
+
+};  
